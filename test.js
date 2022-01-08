274ee5942c10ddb1174b4d2c3d3cc285d6e61683
Manjeet <manjeetkamboj394@gmail.com>
2022-01-08 17:08:14 +0530

    Added main html css and js files

---
+++ test.js
@@ -0,0 +1,370 @@
+$(function() {
+    $('.m1').hover(function() {
+      $('.t2').css('opacity', '50%');
+      $('.t3').css('opacity', '50%');
+      $('.t4').css('opacity', '50%');
+      $('.t5').css('opacity', '50%');
+      $('.t6').css('opacity', '50%');
+      $('.t7').css('opacity', '50%');
+      $('.t8').css('opacity', '50%');
+      $('.t1').css('width', '440');
+      $('.t1').css('height', '440');
+      $('.t1').css('margin-top', '20px');
+      $('.t1').css('margin-left', '20px');
+
+    }, function() {
+      // on mouseout, reset the background colour
+      $('.t2').css('opacity', '');
+      $('.t3').css('opacity', '');
+      $('.t4').css('opacity', '');
+      $('.t5').css('opacity', '');
+      $('.t6').css('opacity', '');
+      $('.t7').css('opacity', '');
+      $('.t8').css('opacity', '');
+      $('.t1').css('width', '');
+      $('.t1').css('height', '');
+      $('.t1').css('margin-top', '');
+      $('.t1').css('margin-left', '');
+    });
+
+    $('.m2').hover(function() {
+      $('.t1').css('opacity', '50%');
+      $('.t3').css('opacity', '50%');
+      $('.t4').css('opacity', '50%');
+      $('.t5').css('opacity', '50%');
+      $('.t6').css('opacity', '50%');
+      $('.t7').css('opacity', '50%');
+      $('.t8').css('opacity', '50%');
+      $('.t2').css('width', '440');
+      $('.t2').css('height', '440');
+      $('.t2').css('margin-top', '20px');
+      $('.t2').css('margin-right', '20px');
+
+    }, function() {
+      // on mouseout, reset the background colour
+      $('.t1').css('opacity', '');
+      $('.t3').css('opacity', '');
+      $('.t4').css('opacity', '');
+      $('.t5').css('opacity', '');
+      $('.t6').css('opacity', '');
+      $('.t7').css('opacity', '');
+      $('.t8').css('opacity', '');
+      $('.t2').css('width', '');
+      $('.t2').css('height', '');
+      $('.t2').css('margin-top', '');
+      $('.t2').css('margin-right', '');
+    });
+
+    $('.m3').hover(function() {
+      $('.t1').css('opacity', '50%');
+      $('.t2').css('opacity', '50%');
+      $('.t4').css('opacity', '50%');
+      $('.t5').css('opacity', '50%');
+      $('.t6').css('opacity', '50%');
+      $('.t7').css('opacity', '50%');
+      $('.t8').css('opacity', '50%');
+      $('.t3').css('width', '440');
+      $('.t3').css('height', '440');
+      $('.t3').css('margin-top', '20px');
+      $('.t3').css('margin-right', '20px');
+
+    }, function() {
+      // on mouseout, reset the background colour
+      $('.t1').css('opacity', '');
+      $('.t2').css('opacity', '');
+      $('.t4').css('opacity', '');
+      $('.t5').css('opacity', '');
+      $('.t6').css('opacity', '');
+      $('.t7').css('opacity', '');
+      $('.t8').css('opacity', '');
+      $('.t3').css('width', '');
+      $('.t3').css('height', '');
+      $('.t3').css('margin-top', '');
+      $('.t3').css('margin-right', '');
+    });
+
+    $('.m4').hover(function() {
+      $('.t1').css('opacity', '50%');
+      $('.t2').css('opacity', '50%');
+      $('.t3').css('opacity', '50%');
+      $('.t5').css('opacity', '50%');
+      $('.t6').css('opacity', '50%');
+      $('.t7').css('opacity', '50%');
+      $('.t8').css('opacity', '50%');
+      $('.t4').css('width', '440');
+      $('.t4').css('height', '440');
+      $('.t4').css('margin-bottom', '20px');
+      $('.t4').css('margin-right', '20px');
+
+    }, function() {
+      // on mouseout, reset the background colour
+      $('.t1').css('opacity', '');
+      $('.t2').css('opacity', '');
+      $('.t3').css('opacity', '');
+      $('.t5').css('opacity', '');
+      $('.t6').css('opacity', '');
+      $('.t7').css('opacity', '');
+      $('.t8').css('opacity', '');
+      $('.t4').css('width', '');
+      $('.t4').css('height', '');
+      $('.t4').css('margin-bottom', '');
+      $('.t4').css('margin-right', '');
+    });
+
+    $('.m5').hover(function() {
+      $('.t1').css('opacity', '50%');
+      $('.t2').css('opacity', '50%');
+      $('.t3').css('opacity', '50%');
+      $('.t4').css('opacity', '50%');
+      $('.t6').css('opacity', '50%');
+      $('.t7').css('opacity', '50%');
+      $('.t8').css('opacity', '50%');
+      $('.t5').css('width', '440');
+      $('.t5').css('height', '440');
+      $('.t5').css('margin-bottom', '20px');
+      $('.t5').css('margin-right', '20px');
+
+    }, function() {
+      // on mouseout, reset the background colour
+      $('.t1').css('opacity', '');
+      $('.t2').css('opacity', '');
+      $('.t3').css('opacity', '');
+      $('.t4').css('opacity', '');
+      $('.t6').css('opacity', '');
+      $('.t7').css('opacity', '');
+      $('.t8').css('opacity', '');
+      $('.t5').css('width', '');
+      $('.t5').css('height', '');
+      $('.t5').css('margin-bottom', '');
+      $('.t5').css('margin-right', '');
+    });
+
+    $('.m6').hover(function() {
+      $('.t1').css('opacity', '50%');
+      $('.t2').css('opacity', '50%');
+      $('.t3').css('opacity', '50%');
+      $('.t4').css('opacity', '50%');
+      $('.t5').css('opacity', '50%');
+      $('.t7').css('opacity', '50%');
+      $('.t8').css('opacity', '50%');
+      $('.t6').css('width', '440');
+      $('.t6').css('height', '440');
+      $('.t6').css('margin-bottom', '20px');
+      $('.t6').css('margin-left', '20px');
+
+    }, function() {
+      // on mouseout, reset the background colour
+      $('.t1').css('opacity', '');
+      $('.t2').css('opacity', '');
+      $('.t3').css('opacity', '');
+      $('.t4').css('opacity', '');
+      $('.t5').css('opacity', '');
+      $('.t7').css('opacity', '');
+      $('.t8').css('opacity', '');
+      $('.t6').css('width', '');
+      $('.t6').css('height', '');
+      $('.t6').css('margin-bottom', '');
+      $('.t6').css('margin-left', '');
+    });
+
+    $('.m7').hover(function() {
+      $('.t1').css('opacity', '50%');
+      $('.t2').css('opacity', '50%');
+      $('.t3').css('opacity', '50%');
+      $('.t4').css('opacity', '50%');
+      $('.t5').css('opacity', '50%');
+      $('.t6').css('opacity', '50%');
+      $('.t8').css('opacity', '50%');
+      $('.t7').css('width', '440');
+      $('.t7').css('height', '440');
+      $('.t7').css('margin-bottom', '20px');
+      $('.t7').css('margin-left', '20px');
+
+    }, function() {
+      // on mouseout, reset the background colour
+      $('.t1').css('opacity', '');
+      $('.t2').css('opacity', '');
+      $('.t3').css('opacity', '');
+      $('.t4').css('opacity', '');
+      $('.t5').css('opacity', '');
+      $('.t6').css('opacity', '');
+      $('.t8').css('opacity', '');
+      $('.t7').css('width', '');
+      $('.t7').css('height', '');
+      $('.t7').css('margin-bottom', '');
+      $('.t7').css('margin-left', '');
+    });
+
+    $('.m8').hover(function() {
+      $('.t1').css('opacity', '50%');
+      $('.t2').css('opacity', '50%');
+      $('.t3').css('opacity', '50%');
+      $('.t4').css('opacity', '50%');
+      $('.t5').css('opacity', '50%');
+      $('.t6').css('opacity', '50%');
+      $('.t7').css('opacity', '50%');
+      $('.t8').css('width', '440');
+      $('.t8').css('height', '440');
+      $('.t8').css('margin-top', '20px');
+      $('.t8').css('margin-left', '20px');
+
+    }, function() {
+      // on mouseout, reset the background colour
+      $('.t1').css('opacity', '');
+      $('.t2').css('opacity', '');
+      $('.t3').css('opacity', '');
+      $('.t4').css('opacity', '');
+      $('.t5').css('opacity', '');
+      $('.t6').css('opacity', '');
+      $('.t7').css('opacity', '');
+      $('.t8').css('width', '');
+      $('.t8').css('height', '');
+      $('.t8').css('margin-top', '');
+      $('.t8').css('margin-left', '');
+    });
+
+    $('#pfp').hover(function() {
+    
+      $('.t1').css('width', '440');
+      $('.t1').css('height', '440');
+      $('.t1').css('margin-top', '20px');
+      $('.t1').css('margin-left', '20px');
+
+
+      $('.t2').css('width', '440');
+      $('.t2').css('height', '440');
+      $('.t2').css('margin-top', '20px');
+      $('.t2').css('margin-right', '20px');
+
+      $('.t3').css('width', '440');
+      $('.t3').css('height', '440');
+      $('.t3').css('margin-top', '20px');
+      $('.t3').css('margin-right', '20px');
+
+      $('.t4').css('width', '440');
+      $('.t4').css('height', '440');
+      $('.t4').css('margin-bottom', '20px');
+      $('.t4').css('margin-right', '20px');
+        
+     
+      $('.t5').css('width', '440');
+      $('.t5').css('height', '440');
+      $('.t5').css('margin-bottom', '20px');
+      $('.t5').css('margin-right', '20px');
+
+     
+      $('.t6').css('width', '440');
+      $('.t6').css('height', '440');
+      $('.t6').css('margin-bottom', '20px');
+      $('.t6').css('margin-left', '20px');
+     
+      $('.t7').css('width', '440');
+      $('.t7').css('height', '440');
+      $('.t7').css('margin-bottom', '20px');
+      $('.t7').css('margin-left', '20px');
+      
+      $('.t8').css('width', '440');
+      $('.t8').css('height', '440');
+      $('.t8').css('margin-top', '20px');
+      $('.t8').css('margin-left', '20px');
+
+    }, function() {
+      // on mouseout, reset the background colour
+     
+      $('.t1').css('width', '');
+      $('.t1').css('height', '');
+      $('.t1').css('margin-top', '');
+      $('.t1').css('margin-left', '');
+
+      $('.t2').css('width', '');
+      $('.t2').css('height', '');
+      $('.t2').css('margin-top', '');
+      $('.t2').css('margin-right', '');
+
+      $('.t3').css('width', '');
+      $('.t3').css('height', '');
+      $('.t3').css('margin-top', '');
+      $('.t3').css('margin-right', '');
+
+      $('.t4').css('width', '');
+      $('.t4').css('height', '');
+      $('.t4').css('margin-bottom', '');
+      $('.t4').css('margin-right', '');
+
+      $('.t5').css('width', '');
+      $('.t5').css('height', '');
+      $('.t5').css('margin-bottom', '');
+      $('.t5').css('margin-right', '');
+
+      $('.t6').css('width', '');
+      $('.t6').css('height', '');
+      $('.t6').css('margin-bottom', '');
+      $('.t6').css('margin-left', '');
+
+      $('.t7').css('width', '');
+      $('.t7').css('height', '');
+      $('.t7').css('margin-bottom', '');
+      $('.t7').css('margin-left', '');
+
+      $('.t8').css('width', '');
+      $('.t8').css('height', '');
+      $('.t8').css('margin-top', '');
+      $('.t8').css('margin-left', '');
+    });
+
+
+    // $('header').hover(function() {   
+    //   $('.title').css('margin-left', '10%');
+    // }, function() {
+    //   // on mouseout, reset the background colour
+    //   $('.title').css('margin-left', '');
+    // });
+    
+  });
+
+//       function idk(n){
+//           if(n==1){
+//       const myTimeout = setTimeout(titleChange, 1000);
+//       function titleChange(){ 
+//             document.getElementById("titlech").innerHTML = "Manjeet - Web Developer";
+//       }
+
+//           }
+//     }
+//     function stop() {
+//   clearTimeout(myTimeout);
+//}
+
+// var mod=0; //0 for dark, 1 for light
+var mod=0;
+
+function mode(){
+    
+    if(mod==0){
+    document.getElementById('body').style.backgroundColor = "white";
+    document.getElementById("titlech").style.color = "black";
+    document.getElementById('h4').style.color = "black";
+    document.getElementById('centre').style.backgroundImage = "url(pictures/bg-black.png)"
+    document.getElementById('web').style.color = "white";
+    document.getElementById('hr1').style.backgroundColor = "black";
+    document.getElementById('mode').innerHTML = "Dark Mode";
+    document.getElementById('mode').style.backgroundColor = "rgb(22, 22, 22)";
+    document.getElementById('mode').style.color = "white";
+    document.getElementById('hire').style.color = "black";
+    mod=1;
+    }
+    else{
+      document.getElementById('body').style.backgroundColor = "rgb(22, 22, 22)";
+    document.getElementById("titlech").style.color = "white";
+    document.getElementById('h4').style.color = "white";
+    document.getElementById('centre').style.backgroundImage = "url(pictures/bg-white.png)"
+    document.getElementById('web').style.color = "black";
+    document.getElementById('hr1').style.backgroundColor = "white";
+    document.getElementById('mode').innerHTML = "Light Mode";
+    document.getElementById('mode').style.backgroundColor = "white";
+    document.getElementById('mode').style.color = "rgb(22, 22, 22)";
+    document.getElementById('hire').style.color = "white";
+    mod=0;
+    }
+   
+}
